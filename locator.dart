165c58becd7cc91317c50dffce347d109e0c1c86
Ahmet Salih Kocatürk <139994163+SalihKocaturk@users.noreply.github.com>
2025-04-29 17:05:41 +0300

    Add files via upload

---
+++ locator.dart
@@ -0,0 +1,13 @@
+import 'package:get_it/get_it.dart';
+import 'package:proje1/domain/cubits/admin_cubit.dart';
+import 'package:proje1/domain/cubits/auth_cubit.dart';
+
+import 'domain/cubits/candidate_cubit.dart';
+
+GetIt getIt = GetIt.instance;
+
+void setupLocators() {
+  getIt.registerSingleton(AdminCubit());
+  getIt.registerSingleton(CandidateCubit());
+  getIt.registerSingleton(AuthCubit());
+}
